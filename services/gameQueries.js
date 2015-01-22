d9a67ac7f0e29367bcab9de1dcde4192da0cbc2d
SVasilev <thesopa@abv.bg>
2015-01-22 23:51:28 +0200

    Works fine with one snake, still have to optimize it more for two snakes.

--- services/gameQueries.js
+++ services/gameQueries.js
@@ -6,24 +6,51 @@ module.exports = {
 		response.end("Invalid Command");
 	},
 
+	// startGame: function(request, response) {
+	// 	this.score = 0;
+	// 	this.ground = new Ground(32, 18);
+	// 	this.snake  = new Snake({x: 5, y: 3}, 2);
+	// 	if(this.ground && this.snake) response.end("OK");
+	// },
+
 	startGame: function(request, response) {
 		this.score = 0;
 		this.ground = new Ground(32, 18);
-		this.snake  = new Snake({x: 5, y: 3}, 2);
-		if(this.ground && this.snake) response.end("OK");
+		if(!this.firstPlayer) {
+			this.firstPlayer = request.ip;
+			this.firstScore  = 0;
+			this.firstSnake  = new Snake({x: 5, y: 3}, 2);
+		}
+		else {
+			if(!this.secondPlayer && request.ip != this.firstPlayer /*USE IT WHEN YOU TEST WITH TWO DIFFERENT IPS*/) {
+				this.secondPlayer = request.ip;
+				this.secondScore  = 0;
+				this.secondSnake  = new Snake({x: 5, y: 13}, 2);
+			}
+		}
+		console.log(this.firstPlayer);
+		console.log(this.secondPlayer);
+		if(this.secondPlayer) response.end("StartGame");
+		else response.end("Waiting");
 	},
 
 	updateGroundState: function(request, response) {
-		if(this.snake && this.ground) {
-			if(this.snake.move(this) && this.ground.refresh(this)) response.end(JSON.stringify(this.ground.array));
+		if(request.ip == this.firstPlayer) {
+			if(this.firstSnake.move(this) && this.ground.refresh(this)) response.end(JSON.stringify(this.ground.array));
+			else this.firstPlayer = undefined;
 			response.end("Game ended with score: " + this.score);
 		}
 		response.end("");
 	},
 
 	setDirection: function(request, response, direction) {
-		if(this.snake) this.snake.setDirection(direction);
+		if(request.ip == this.firstPlayer) this.firstSnake.setDirection(direction);
 		response.end("");
+	},
+
+	getPlayersCount: function(request, response) {
+		if(this.firstPlayer) response.end("1");
+		else response.end("0");
 	},
 
 	executeCommand: function(request, response) {
@@ -31,17 +58,11 @@ module.exports = {
 		case "startGame":
 			this.startGame(request, response);
 			break;
-		case "getGroundState":
-			this.getGroundState(request, response);
-			break;
-		case "getSnakeState":
-			this.getSnakeState(request, response);
-			break;
 		case "updateGroundState":
 			this.updateGroundState(request, response);
 			break;
-		case "getFruitCoordinates":
-			this.getFruitCoordinates(request, response);
+		case "getPlayersCount":
+			this.getPlayersCount(request, response);
 			break;
 		case "up":
 		case "down":
